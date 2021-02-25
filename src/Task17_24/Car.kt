81b5e17d90112d0d8e3e07365696ab8fbd1fc1ac
Lyapk <Elena525658>
2021-02-26 00:49:40 +0300

    Added task 23

---
+++ src/Task17_24/Car.kt
@@ -0,0 +1,3 @@
+package Task17_24
+
+data class Car(val brand: String, var color: String, var number: String)
--- src/Task17_24/Main.kt
+++ src/Task17_24/Main.kt
@@ -2,4 +2,6 @@ package Task17_24
 
 fun main(args: Array<String>) {
     var boeing747: Boeing747 = Boeing747(2, 7500, 270, 200)
+    val car = Car("Porsche", "Black", "qw123e")
+    print(car.toString())
 }
