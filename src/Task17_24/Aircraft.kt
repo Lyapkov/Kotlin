3d9f8b9021ab0508bcbcee2294d4f858c8cbf585
Lyapk <Elena525658>
2021-02-26 00:05:39 +0300

    Added task 21

--- src/Task17_24/Aircraft.kt
+++ src/Task17_24/Aircraft.kt
@@ -1,6 +1,6 @@
 package Task17_24
 
-open class Aircraft(var aircraftNumber: Int, var maximumFlightRange: Int, var tankCapacity: Int) {
+abstract class Aircraft(var aircraftNumber: Int, var maximumFlightRange: Int, var tankCapacity: Int) {
     var fuelConsumption: Int? = null
         get() = tankCapacity / maximumFlightRange * 100
 }
